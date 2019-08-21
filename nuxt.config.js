39a40e29ad7c65ea96dcd87e01700dc994b39565
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-08-21 19:39:59 +0900

    [Remove] Default Nuxt.js Icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,6 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' },
     ],
     script: [
