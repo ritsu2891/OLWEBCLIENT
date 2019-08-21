4e62be37d6f386832f43360f34ce3cbf890c3b85
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-08-21 23:21:26 +0900

    [Change] Export API ENDPOINT.

--- components/API.js
+++ components/API.js
@@ -1,18 +1,16 @@
-export default {requestDBData}
-
 import axios from 'axios'
 
 const APIURI = 'http://localhost:8025'
-const ENDPOINT = {
-    rawSignal: '/rawdata',
-    updateDateTime: '/updatedatetime',
+export const ENDPOINT = {
+    rawSignal: 'rawdata',
+    updateDateTime: 'updatedatetime',
 };
 
 function getUrl(which) {
-    return APIURI + ENDPOINT[which];
+    return `${APIURI}/${ENDPOINT[which]}`;
 }
 
-async function requestDBData(which) {
+export async function requestDBData(which) {
     var res = await axios.get(getUrl(which));
     return res.data;
 }
