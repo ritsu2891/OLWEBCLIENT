1b94bff548682a599d0db3f7e31ea7818aef58ab
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-08-21 19:40:22 +0900

    [Change] Separate ChartJsGraph into Vue module.

---
+++ components/ChartJsGraph.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <canvas id="graph"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+  nsme: 'chart-js-graph',
+  data: function() {
+    return {
+      myChart: NaN
+    };
+  },
+  mounted() {
+    var ctx = document.getElementById('graph').getContext('2d');
+    this.myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          label: '波形',
+          data: [],
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          pointRadius: 0,
+        }],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            gridLines: {
+                drawOnChartArea: false
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+                drawOnChartArea: false
+            }
+          }]
+        },
+      }
+    });
+
+    this.$axios.$get('http://localhost:8025/rawdata').then((res) => {
+      var n = res.data.length;
+      for (var i = 0; i < n; i++) {
+        this.myChart.data.labels.push('');
+        this.myChart.data.datasets[0].data.push(res.data[i]);
+      }
+      this.myChart.update();
+    });
+
+    // let self = this;
+    window.setInterval(() => {
+      this.myChart.data.labels = [];
+      this.myChart.data.datasets[0].data = [];
+      this.$axios.$get('http://localhost:8025/rawdata').then((res) => {
+        var n = res.data.length;
+        for (var i = 0; i < n; i++) {
+          this.myChart.data.labels.push('');
+          this.myChart.data.datasets[0].data.push(res.data[i]);
+        }
+        this.myChart.update();
+      });
+    }, 5000);
+  }
+}
+</script>
+<style lang="scss" scoped>
+#graph {
+    width: 1000px;
+    height: 500px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,89 +2,23 @@
   <div class="container">
     <div class="row justify-content-md-center">
       <div class="col-auto">
-        <canvas id="rawdataGraph"></canvas>
+        <chart-js-graph></chart-js-graph>
       </div>
     </div>
   </div>
 </template>
-
 <script>
+import ChartJsGraph from '~/components/ChartJsGraph.vue';
 
 export default {
-  // components: {
-  //   Logo
-  // },
-  data: function() {
-    return {
-      mydata: [],
-      mylabel: [],
-      myChart: NaN
-    };
+  components: {
+    ChartJsGraph,
   },
-  mounted() {
-    var ctx = document.getElementById('rawdataGraph').getContext('2d');
-    this.myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: this.mylabel,
-        datasets: [{
-          label: '# of Votes',
-          data: this.mydata,
-          backgroundColor: 'rgba(0, 0, 0, 0)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          pointRadius: 0,
-        }],
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            gridLines: {
-                drawOnChartArea: false
-            }
-          }],
-          yAxes: [{
-            gridLines: {
-                drawOnChartArea: false
-            }
-          }]
-        },
-      }
-    });
-
-    this.$axios.$get('http://localhost:8025/rawdata').then((res) => {
-      var n = res.data.length;
-      for (var i = 0; i < n; i++) {
-        this.mylabel.push('');
-        this.mydata.push(res.data[i]);
-      }
-      this.myChart.update();
-    });
-
-    // let self = this;
-    window.setInterval(() => {
-      this.mylabel = [];
-      this.myChart.data.datasets[0].data = [];
-      this.$axios.$get('http://localhost:8025/rawdata').then((res) => {
-        var n = res.data.length;
-        for (var i = 0; i < n; i++) {
-          this.mylabel.push('');
-          this.myChart.data.datasets[0].data.push(res.data[i]);
-        }
-        this.myChart.update();
-      });
-    }, 5000);
-  }
 }
 </script>
 <style lang="scss">
   #container {
     width: 100%;
     height: 100%;
-  }
-
-  #rawdataGraph {
-    width: 1000px;
-    height: 600px;
-    // background-color: aqua;
   }
 </style>
