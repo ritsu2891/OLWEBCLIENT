2934da1c2bb1df248b9d9994524e07d122d84899
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-09-19 15:24:50 +0900

    [Add] 2 people ARS result view

--- components/ValueCard.vue
+++ components/ValueCard.vue
@@ -1,53 +1,69 @@
 <template>
-    <div class="row valueCard">
-        <div class="col align-self-center" style="">
-            <div class="valueCard__label">{{label}}</div>
-            <div class="valueCard__val" :id="`value-${label}`">{{val}}</div>
-        </div>
+  <div class="row valueCard">
+    <div class="col align-self-center" style>
+      <div class="valueCard__label">{{label}}</div>
+      <div class="valueCard__val" :id="`value-${label}`">{{val}}</div>
     </div>
+  </div>
 </template>
 <script>
-import GraphDataManager from '~/components/GraphDataManager.js'
+import GraphDataManager from "~/components/GraphDataManager.js";
 
 export default {
-    name: 'value-card',
-    data: function() {
-        return {
-            endpoint: 'ars',
-            ds: {data: []}
-        }
+  name: "value-card",
+  data: function() {
+    return {
+      endpoint: "ars",
+      ds: { data: [] }
+    };
+  },
+  props: {
+    label: {
+      type: String,
+      default: 'ARS'
     },
-    props: [
-        'label'
-    ],
-    computed: {
-        val: function () {
-            return this.ds.data[0]
-        }
-    },
-    mounted: function() {
-        // 一時措置
-        GraphDataManager.startManageV(this, this.ds, 'ars', 'replaceAll');
+    n: {
+      type: Number,
+      default: 1
     }
-}
+  },
+  computed: {
+    val: function() {
+      if (this.ds.data && this.ds.data[0]) {
+        return Math.round(this.ds.data[0]) * 0.01 + "s";
+      } else {
+        return "---";
+      }
+    }
+  },
+  mounted: function() {
+    // 一時措置
+    if (this.n == 1) {
+      GraphDataManager.startManageV(this, this.ds, "ars", "replaceAll");
+    } else if (this.n == 2) {
+      GraphDataManager.startManageV(this, this.ds, 'ars2', 'replaceAll');
+    }
+  }
+};
 </script>
 <style lang="scss">
-    .valueCard {
-        width: 300px;
-        height: 300px;
-        border: 1px solid #cccccc;
-        & * {
-            text-align: center;
-        }
-        &__label, &__val {
-            text-align: center;
-            width: 100%;
-        }
-        &__label {
-            font-size: 30px;
-        }
-        &__val {
-            font-size: 150px;
-        }
-    }
+.valueCard {
+  width: 500px;
+  height: 300px;
+  border: 1px solid #cccccc;
+  & * {
+    text-align: center;
+  }
+  &__label,
+  &__val {
+    text-align: center;
+    width: 100%;
+  }
+  &__label {
+    font-size: 30px;
+  }
+  &__val {
+    font-size: 150px;
+  }
+}
 </style>
