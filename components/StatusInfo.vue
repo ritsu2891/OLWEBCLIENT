33dc50c68ad6e0da42ae14f29b0ba13135fc52ea
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-09-23 23:14:19 +0900

    [Change] Enhance Status Info

--- components/StatusInfo.vue
+++ components/StatusInfo.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="statusInfo shadow">
-    <div class="statusInfo__status">問題なし</div>
-    <div class="statusInfo__description">列車に異常は検知されていません</div>
+    <div class="statusInfo__status">{{status}}</div>
+    <hr v-if="detail" />
+    <div class="row align-items-center" v-if="detail">
+      <div class="col-auto pr-3" v-if="detailImage">
+        <img class="statusInfo__image" :src="detailImage" />
+      </div>
+      <div class="col pl-0">
+        <div class="statusInfo__description">{{detail}}</div>
+      </div>
+    </div>
+    <hr v-if="dlDataSet" />
+    <DLResultView v-bind="dlDataSet" v-if="dlDataSet"></DLResultView>
+    <hr v-if="rangeDataSet" />
+    <AbnormalRangeMap
+      :zoom="10"
+      style="height: 200px;"
+      :start-pos="rangeDataSet.startPos"
+      :end-pos="rangeDataSet.endPos"
+      v-if="rangeDataSet"
+    ></AbnormalRangeMap>
   </div>
 </template>
+<script>
+import DLResultView from "~/components/DLResultView.vue";
+import AbnormalRangeMap from "~/components/AbnormalRangeMap.vue";
+
+export default {
+  components: { DLResultView, AbnormalRangeMap },
+  props: {
+    status: { type: String, default: "状況不明" },
+    detail: { type: String }, //車輪に異物が挟まっている可能性があります
+    detailImage: { type: String }, ///wheelzoom.png
+    dlDataSet: {},
+    rangeDataSet: {}
+  }
+};
+</script>
 <style lang="scss">
 .statusInfo {
   margin-left: 30px;
@@ -16,6 +49,18 @@
   }
   &__description {
     font-size: 30px;
+  }
+  &__image {
+    width: 90px;
+  }
+  & hr {
+    margin: {
+      top: 5px;
+      bottom: 5px;
+    }
+    border-top: {
+      color: black;
+    }
   }
 }
 </style>
--- pages/yamashita.vue
+++ pages/yamashita.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="container">
-    <div class="timelineContainer">
-      <div class="timeline">
-        <canvas id="canvas"></canvas>
+    <button v-on:click="add">追加</button>
+    <div class="timeline">
+      <LiveMap
+        class="primaryMap"
+        width="100%"
+        style="height: 600px; width: 100%;"
+        :mapCenter="[136.893865, 35.157141]"
+        :zoom="11.8"
+      ></LiveMap>
+      <div style="width: 5000px">
+        <canvas id="canvas" style="margin-top: -340px;"></canvas>
         <div class="statusInfoCardLine">
-          <TimeLabeledContent time="LIVE" content="StatusInfo"></TimeLabeledContent>
-          <TimeLabeledContent time="17:45" content="StatusInfo" icon="▲" icon-text-color="red"></TimeLabeledContent>
-          
+          <transition-group name="fade">
+            <TimeLabeledContent
+              v-for="(item, index) in timeline"
+              :key="`timelineitem${item.id}`"
+              :time="item.time"
+              content="StatusInfo"
+              :contentAttr="item.contentAttr"
+              :icon="item.icon"
+              :icon-text-color="item.iconTextColor"
+              :class="{'trans': trans}"
+            ></TimeLabeledContent>
+          </transition-group>
         </div>
       </div>
     </div>
@@ -17,10 +34,19 @@ import * as PIXI from "pixi.js";
 
 import StatusInfo from "~/components/StatusInfo.vue";
 import TimeLabeledContent from "~/components/TimeLabeledContent.vue";
+import MapTest from "~/components/MapTest.vue";
+import BaseMap from "~/components/BaseMap.vue";
+import LiveMap from "~/components/LiveMap.vue";
+import AbnormalRangeMap from "~/components/AbnormalRangeMap.vue";
 
 export default {
   components: {
-    StatusInfo, TimeLabeledContent
+    StatusInfo,
+    TimeLabeledContent,
+    MapTest,
+    BaseMap,
+    LiveMap,
+    AbnormalRangeMap
   },
   mounted: function() {
     let app = new PIXI.Application({
@@ -29,7 +55,8 @@ export default {
       antialias: true,
       view: document.getElementById("canvas"),
       //   backgroundColor: 0xdddddd,
-      backgroundColor: 0xffffff
+      // backgroundColor: 0xffffff
+      transparent: true
     });
 
     function addWheel(x, y) {
@@ -96,21 +123,102 @@ export default {
     //   var y = app.renderer.plugins.interaction.mouse.global.y;
     //   console.log(`${x}, ${y}`);
     // });
+  },
+  data() {
+    return {
+      timeline: [
+        {
+          id: 34,
+          time: "LIVE",
+          icon: "●",
+          iconTextColor: "green",
+          contentAttr: { status: "正常" }
+        },
+        {
+          id: 35,
+          time: "17:45",
+          icon: "▲",
+          iconTextColor: "red",
+          contentAttr: {
+            status: "異物検知",
+            detail: "車輪に異物が挟まっている可能性があります",
+            detailImage: "wheelzoom.png",
+            dlDataSet: {
+              labels: ["針金", "抵抗", "アイテム3"],
+              datas: [10, 81, 9],
+              images: ["/針金.png", "/抵抗.jpg", "/針金.png"]
+            },
+            rangeDataSet: {
+              startPos: [136.90104246139526, 35.142985260896246],
+              endPos: [136.91048383712769, 35.12066327340687]
+            }
+          }
+        },
+        {
+          id: 36,
+          time: "13:21",
+          icon: "▲",
+          iconTextColor: "red",
+          contentAttr: {
+            status: "異物検知",
+            detail: "車輪に異物が挟まっている可能性があります",
+            detailImage: "wheelzoom.png",
+            dlDataSet: {
+              labels: ["針金", "抵抗", "アイテム3"],
+              datas: [90, 7, 3],
+              images: ["/針金.png", "/抵抗.jpg", "/針金.png"]
+            },
+            rangeDataSet: {
+              startPos: [136.94571733474731, 35.12073347792679],
+              endPos: [136.95884943008423, 35.13508903084878]
+            }
+          }
+        }
+      ],
+      trans: false
+    };
+  },
+  methods: {
+    add() {
+      this.trans = true;
+      const self = this;
+      setTimeout(function() {
+        self.trans = false;
+        self.timeline.unshift({
+          id: 31,
+          time: "LIVE",
+          icon: "●",
+          iconTextColor: "green",
+          contentAttr: { status: "正常" }
+        });
+      }, 300);
+    }
   }
 };
 </script>
 <style lang="scss">
 .timelineContainer {
-  overflow: scroll;
-  height: 700px;
+  overflow-x: scroll;
+}
+
+.trans {
+  transition: all 200ms 0s ease;
+  transform: translateX(405px);
 }
 
 .timeline {
-  width: 1500px;
+  width: 100%;
+  overflow-x: scroll;
+  overflow-y: hidden;
 }
 
 .statusInfoCardLine {
   position: relative;
   top: -65px;
+  height: 830px;
+}
+
+.primaryMap .mapboxgl-canvas {
+  z-index: -1;
 }
 </style>
