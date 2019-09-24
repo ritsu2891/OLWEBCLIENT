ad0ac5f2184a376a3644b4004693b89e1ceae5a4
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-09-25 01:30:05 +0900

    [Change] Separate logic

--- pages/test.vue
+++ pages/test.vue
@@ -12,18 +12,27 @@
     <h1>TimeLine</h1>
     
     <TimeLine>
-      <TimeLabeledContent v-for="(item, idx) in timelineItem" :key="`tli${idx}`"></TimeLabeledContent>
+      <TimeLabeledContent v-for="(item, idx) in timelineItem" :key="`tli${idx}`">
+        <ValueCard class="shadow bg-light m-3"></ValueCard>
+      </TimeLabeledContent>
     </TimeLine>
 
     <button v-on:click="addTimelineItem">追加</button>
+
+    <h1>ValueCrad</h1>
+
+    <ValueCard :label="テスト項目" val="129.2s"></ValueCard>
+    <ValueCard :label="テスト項目" val="29.2s" class="shadow bg-light m-3" style="width: 400px; display: inline-block;"></ValueCard>
+    <ValueCard :label="テスト項目" val="29.2s" class="shadow bg-light m-3" style="width: 400px; display: inline-block;"></ValueCard>
   </div>
 </template>
 <script>
 import TimeLabeledContent from "~/components/SharedUIComponents/TimeLabeledContent.vue";
 import TimeLine from "~/components/SharedUIComponents/TimeLine.vue";
+import ValueCard from "~/components/SharedUIComponents/ValueCard.vue";
 
 export default {
-  components: { TimeLabeledContent, TimeLine },
+  components: { TimeLabeledContent, TimeLine, ValueCard },
   data: function() {
     return {
       timelineItem: [1, 2, 3]
