3316d8379945bcbe3ed5fa9e6779e0f8d43af2a0
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-09-25 01:30:05 +0900

    [Change] Separate logic

--- components/SharedUIComponents/ValueCard.vue
+++ components/SharedUIComponents/ValueCard.vue
@@ -11,46 +11,18 @@ import GraphDataManager from "~/components/GraphDataManager.js";
 
 export default {
   name: "value-card",
-  data: function() {
-    return {
-      endpoint: "ars",
-      ds: { data: [] }
-    };
-  },
   props: {
     label: {
       type: String,
       default: 'ARS'
     },
-    n: {
-      type: Number,
-      default: 1
-    }
+    val: { default: '---' }
   },
-  computed: {
-    val: function() {
-      if (this.ds.data && this.ds.data[0]) {
-        return Math.round(this.ds.data[0]) * 0.01 + "s";
-      } else {
-        return "---";
-      }
-    }
-  },
-  mounted: function() {
-    // 一時措置
-    if (this.n == 1) {
-      GraphDataManager.startManageV(this, this.ds, "ars", "replaceAll");
-    } else if (this.n == 2) {
-      GraphDataManager.startManageV(this, this.ds, 'ars2', 'replaceAll');
-    }
-  }
 };
 </script>
 <style lang="scss">
 .valueCard {
-  width: 500px;
-  height: 300px;
-  border: 1px solid #cccccc;
+  // border: 1px solid #cccccc;
   & * {
     text-align: center;
   }
