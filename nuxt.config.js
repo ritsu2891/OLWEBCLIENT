82dd40b8e92ab5cfa4bd5493c270f3449825953f
Ritsuki KOKUBO <ritsu2891@gmail.com>
2019-09-29 23:37:42 +0900

    [Change] Update dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,6 @@ export default {
       { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' },
       { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' },
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js' }
     ]
   },
   /*
